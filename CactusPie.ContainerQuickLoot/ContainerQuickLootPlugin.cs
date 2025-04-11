14eac3cd3d69e5b45b3768a5f4e462ce5280b644
garlicbreadtcg <colekatk@gmail.com>
2025-04-10 22:20:11 -0500

    Updated version
    Added libs for dependencies directly.

--- CactusPie.ContainerQuickLoot/ContainerQuickLootPlugin.cs
+++ CactusPie.ContainerQuickLoot/ContainerQuickLootPlugin.cs
@@ -4,7 +4,7 @@ using JetBrains.Annotations;
 
 namespace CactusPie.ContainerQuickLoot
 {
-    [BepInPlugin("com.cactuspie.containerquikloot", "CactusPie.ContainerQuickLoot", "1.6.0")]
+    [BepInPlugin("com.cactuspie.containerquikloot", "CactusPie.ContainerQuickLoot", "1.7.0")]
     public class ContainerQuickLootPlugin : BaseUnityPlugin
     {
         internal static ConfigEntry<bool> EnableForCtrlClick { get; set; }
